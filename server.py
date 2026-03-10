88ba3f50fd17e13c48d5b21e195df60c00d5f9e1
Your Name <javeria.asghar194@gmail.com>
2026-03-10 11:09:13 +0500

    Fix server

--- server.py
+++ server.py
@@ -71,4 +71,4 @@ def analyze():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=False)
